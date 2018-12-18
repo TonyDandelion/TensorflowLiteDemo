a82dadf653a83b133e8fd3c9cc79c69fa2cc324e
unknown <845523085@qq.com>
2018-12-18 13:47:33 +0800

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# TensorflowLiteDemo
+
+## reference Link
+
+[CV深度学习模型Android端落地方案之三：使用Tensorflow Lite 将自己的训练得到的模型移植到Android上](https://blog.csdn.net/tonydandelion2014/article/details/85058657)
---
+++ app/src/main/java/com/example/tommy/tensorflowlitedemo/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.tommy.tensorflowlitedemo;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");//可以去掉
+    }
+
+    private static final String TAG = "MainActivity";
+    private static final String MODEL_FILE = "file:///android_asset/mnist.pb"; //模型存放路径
+    TextView txt;
+    TextView tv;
+    ImageView imageView;
+    Bitmap bitmap;
+    PredictionTF preTF;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Example of a call to a native method
+        tv = (TextView) findViewById(R.id.sample_text);
+        txt=(TextView)findViewById(R.id.txt_id);
+        imageView =(ImageView)findViewById(R.id.imageView1);
+        bitmap = BitmapFactory.decodeStream(getClass().getResourceAsStream("/res/drawable/test.bmp"));
+        imageView.setImageBitmap(bitmap);
+        preTF = new PredictionTF(getAssets(),MODEL_FILE);//输入模型存放路径，并加载TensoFlow模型
+    }
+
+    public void click01(View v){
+        String res="预测结果为：";
+        int[] result= preTF.getPredict(bitmap);
+        for (int i=0;i<result.length;i++){
+            Log.i(TAG, res+result[i] );
+            res=res+String.valueOf(result[i])+" ";
+        }
+        txt.setText(res);
+    }
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+}
